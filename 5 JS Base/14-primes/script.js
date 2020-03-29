013221882802b61c1adb70dddc950b2cc046cd8c
Pyotr <2chilavert3@mail.ru>
2020-03-29 15:05:33 +0300

    Решена задача №14 «Простые числа».

--- 5 JS Base/14-primes/script.js
+++ 5 JS Base/14-primes/script.js
@@ -7,7 +7,29 @@
 */
 
 function primes(num) {
-    // Напишите код здесь
+    const primes = [];
+    for (let i = 2; i <= num; i++){
+        if (isPrime(i)){
+            primes.push(i);            
+        }
+    }
+    return primes;
+}
+
+function isPrime(n) {  
+    if (!Number.isInteger(n))
+        return 'Входной параметр должен быть целым числом';  
+    if (n <= 1) 
+        return false;   
+    if (n === 2)
+        return true;
+    if (n % 2 == 0)
+        return false;
+    for (let i = 3; i < n; i += 2){
+        if (n % i == 0)
+            return false;
+    }
+    return true;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
