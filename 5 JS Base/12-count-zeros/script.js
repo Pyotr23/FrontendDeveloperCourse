fd531583461cddf75d15de9cce54986713d2ebcf
Pyotr <2chilavert3@mail.ru>
2020-03-29 14:42:02 +0300

    Решена рекурсивно задача №12 «Посчитать количество нулей».

--- 5 JS Base/12-count-zeros/script.js
+++ 5 JS Base/12-count-zeros/script.js
@@ -7,7 +7,20 @@
 */
 
 function countZeros(n) {
-    // Напишите код здесь
+    if (!Number.isInteger(n) || n < 0)
+        return 'Входной параметр должен быть неотрицательным целым числом.';    
+    let count = 0;    
+    for (let i = 9; i <= n; i++){
+        count += countZerosInNumber(i);
+    }
+    return count;
+}
+
+function countZerosInNumber(n){    
+    const isZeroLastDigit = n % 10 === 0 ? 1 : 0;
+    return n / 10 < 10  
+        ? isZeroLastDigit
+        : isZeroLastDigit + countZerosInNumber(Math.floor(n / 10));    
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
