53bdb049753218dcd27512508fd60736e21257e2
Pyotr <2chilavert3@mail.ru>
2020-04-14 09:04:15 +0300

    Работа принята, но есть замечания.

--- 6 DOM/script.js
+++ 6 DOM/script.js
@@ -1,3 +1,8 @@
+
+// Можно лучше (минута делов):   перенесите  в отдельный файл, меньше строк, больше понимание, 
+// Правильная организация кода, это важная часть разработки. Ведь код надо постоянно поддерживать
+// подключить его можно через  <script src="js/initialCards.js"></script> 
+// Плюс мы выносим данные отдельно, а логика останется в этом файле 
 const initialCards = [
     {
       name: 'Архыз',
@@ -60,6 +65,25 @@ function randomFillPlaces(){
 }
 
 function createCard(cardName, link){
+   /*  Можно лучше: 
+   *  Альтернативный способ создания карточки. При нем не требуется создавать вручную все
+   * Вы можете реализовать функцию, которая сразу же возвращает “кусок” разметки. Это решает проблему постоянного createElement DOM-элементов. 
+    function getTemplate(data){ 
+      const template = `<div class="place-card"> 
+                  <div class="place-card__image" style="background: url(${data.link})"> 
+                    <button class="place-card__delete-icon"></button>
+                  </div>
+                  <div class="place-card__description">
+                    <h3 class="place-card__name">${data.name}</h3>
+                    <button class="place-card__like-icon"></button>
+                  </div>
+                </div>`
+    return template;
+    } 
+   *  Этот кусок разметки в дальнейшем можно вставить в DOM, воспользовавшись методом insertAdjacentHTML().
+   *  https: //developer.mozilla.org/ru/docs/Web/API/Element/insertAdjacentHTML
+   *    pointElement.insertAdjacentHTML('afterend', getTemplate(data));
+   */ 
     const imageElement = createElement('div', 'place-card__image');    
     imageElement.setAttribute('style', `background-image: url(${link})`);
     const deleteButton = createElement('button', 'place-card__delete-icon');
@@ -105,6 +129,21 @@ event.preventDefault();
 const form = document.forms.new;
 const placeName = form.elements.name.value;
 const placeLink = form.elements.link.value;
+// можно лучше: Для валидации используйте кастомный метод validation
+// https: //developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5/Constraint_validation#Constraint_API%27s_element.setCustomValidity() 
+// на русском https: //msiter.ru/tutorials/javascript/js_validation 
+// на русском https://htmlacademy.ru/blog/useful/html/form-validation-techniques 
+// на английском очень хорошая статья с примерами https://css-tricks.com/form-validation-part-2-constraint-validation-api-javascript/ 
+// 
+// как пример, если вы установите  <input type="text" min="10" max="100" >
+// то сразу сможете определить что текст слишком короткий, например так: 
+//  
+// if (validity.tooShort) { 
+// // Значение слишком короткое 
+// }
+// if (validity.tooLong) { 
+// // Значение слишком длинное 
+// }
 if (placeName.length !== 0 && placeLink.length !== 0){    
   closeForm();
   const newCard = createCard(placeName, placeLink);
@@ -114,3 +153,25 @@ if (placeName.length !== 0 && placeLink.length !== 0){
 })
 
 randomFillPlaces();
+
+ /**
+ * Здравствуйте.
+ * --------------------------------------------------------------------
+ * Весь функционал работает корректно 
+ * Код чистый и хорошо читается 
+ * Вы используете логические группировки операций 
+ * У вас нет дублирование кода
+ *  Вы не используете небезопасный innerHtml
+ *  Вы используете делегирование
+    
+  * можно лучше: избегайте сложных условий и большой вложенности в условии. Чем сложнее условие, чем больше
+  * вложенности в условии, тем сложнее анализировать код, сложнее искать ошибки и поддерживать такой код
+  * самый простой вариант это убирать условия или блок в условии в отдельную функцию
+ *
+ * можно лучше: Старайтесь не объявлять большое количество переменных. Чем больше переменных, тем сложнее понимать за что они 
+ * отвечают и какую полезную нагрузку несут в коде. Лучше инкапсулировать(прятать) переменные в функциях. 
+ * В будущем вам проще будет искать ошибки и разбираться в сложных взаимосвязях
+ *
+ * работа принимается
+ * 
+ */
