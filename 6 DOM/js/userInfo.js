0363b52d324296f2e84e29ad0b7dc87fe3fe8b2a
Pyotr <2chilavert3@mail.ru>
2020-05-27 23:40:42 +0300

    Исправления в методе UserInfo.

--- 6 DOM/js/userInfo.js
+++ 6 DOM/js/userInfo.js
@@ -1,22 +1,32 @@
 class UserInfo {
     constructor(container) {
-        this.container = container;
-        this.name = container.querySelector('.user-info__name').textContent;
-        this.job = container.querySelector('.user-info__job').textContent;
+        this._container = container;        
+        this._nameElement = container.querySelector('.user-info__name');
+        this._jobElement = container.querySelector('.user-info__job');
+        this._name = this._nameElement.textContent;
+        this._job = this._jobElement.textContent;
     }
 
     set(name, job) {
-        this.name = name;
-        this.job = job;
+        this._name = name;
+        this._job = job;
+    }   
+
+    get name() {
+        return this._name;
+    }
+
+    get job() {
+        return this._job;
     }
 
     update() {
-        /*
+        /*  !!! DONE !!!
             Надо исправить: Dom-элементы user-info__name и user-info__job уже искались в конструкторе.
             Стоит dom-элемент присваивать какой-либо переменной (например, this.nameElement)
             и затем использовать её в текущем методе. Аналогично для job.
          */
-        this.container.querySelector('.user-info__name').textContent = this.name;
-        this.container.querySelector('.user-info__job').textContent = this.job;
+        this._nameElement.textContent = this._name;
+        this._jobElement.textContent = this._job;
     }
 }
